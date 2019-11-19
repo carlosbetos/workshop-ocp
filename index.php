bc29175c5957a801488f6a7e593e14642868b389
carlosbetos <carlosbetos@gmail.com>
2019-11-19 15:45:25 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php                    
   echo "<h1>Openshift Workshop v1.0</h1> ";                    
   $host= gethostname();
-  $ip = gethostbyname($host);                    
+  $ip = gethostbyname($host);  
+ $host= gethostname();
+  $ip = gethostbyname($host);      
   echo $ip;
   ?>
